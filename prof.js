00b6c93619bd55140fe92f563065fef7a0491741
chandrika-120306 <239x1a3321@gprec.ac.in>
2025-12-19 21:56:04 +0530

    save

---
+++ prof.js
@@ -0,0 +1,12 @@
+<script>
+  function showInfo() {
+    alert('Name: John Doe
+Mobile: 9876543210');
+    // Later you can replace alert with a modal or a div showing details
+  }
+
+  function logoutUser() {
+    alert('Logged out!');
+    // Here you would clear session / redirect in a real app
+  }
+</script>
